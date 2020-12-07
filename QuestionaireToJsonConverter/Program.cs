ce09f442b16c91567431b16993ab8f8817a0b80a
Marius Krause <mariuskrause90@gmail.com>
2020-12-07 11:59:11 +0100

    init commit

---
+++ QuestionaireToJsonConverter/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace QuestionaireToJsonConverter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var reader = new StreamReader($@"C:\Datensatzerstellung.csv"))
+            {
+                //while (!reader.EndOfStream)
+                //{
+                    var questions = reader.ReadLine();
+
+                    var intents = GetIntents(questions);
+
+                    var values = questions.Split(',');
+                //}
+            }
+        }
+
+        static private List<string> GetIntents(string questions)
+        {
+            var a = Regex.Matches(questions, @"~~(.+?)~~");
+            return a.Select(bla => bla.Groups[1].Value).ToList();
+
+        }
+    }
+}
